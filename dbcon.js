0e5e8abb69510052031ca8f722901ad95c114e5f
Nick Van Domelen <30646801+Nick-Van-Domelen@users.noreply.github.com>
2021-07-21 19:12:17 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_vandomni',
-  password        : '6394',
+  //password        : '',
   database        : 'cs340_vandomni'
 });
 module.exports.pool = pool;
